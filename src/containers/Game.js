aad078805d5ae674c7619e53cf14776c1106a93f
Haakan <haakandev@gmail.com>
2019-09-28 20:47:00 +1000

    add naive game logic, with default snippets

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,17 +1,55 @@
-import React, { useState } from 'react';
-import { Container, Snippet } from '../components';
+import React, { useEffect, useRef, useState } from 'react';
+import { Container, Scoreboard, Snippet } from '../components';
+import defaultSnippets from '../defaultSnippets';
+
+const snippetKeys = Object.keys(defaultSnippets);
+const snippets = defaultSnippets;
+
+const getRandomSnippetKey = () => snippetKeys[Math.floor(Math.random() * snippetKeys.length)];
 
 const Game = () => {
-  const [currentInput, setCurrentInput] = useState('');
+  const inputEl = useRef(null);
+  const [currentSnippetKey, setCurrentSnippetKey] = useState(null);
+  const [currentScore, setCurrentScore] = useState({ completed: 0, failedAttempts: 0 });
+  const [usedSnippets, setUsedSnippets] = useState([]);
+
+  useEffect(() => {
+    const onSubmit = event => {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        if (inputEl.current.value === snippets[currentSnippetKey].prefix) {
+          setUsedSnippets(k => [...k, currentSnippetKey]);
+          setCurrentScore(oldScore => ({ ...oldScore, completed: oldScore.completed + 1 }));
+          inputEl.current.value = '';
+        } else {
+          setCurrentScore(oldScore => ({
+            ...oldScore,
+            failedAttempts: oldScore.failedAttempts + 1,
+          }));
+        }
+      }
+    };
+    window.addEventListener('keyup', onSubmit);
+    return () => {
+      window.removeEventListener('keyup', onSubmit);
+    };
+  }, [currentSnippetKey]);
+
+  useEffect(() => {
+    let nextSnippetKey = getRandomSnippetKey();
+    while (usedSnippets.includes(nextSnippetKey)) {
+      nextSnippetKey = getRandomSnippetKey();
+    }
+    setCurrentSnippetKey(nextSnippetKey);
+  }, [usedSnippets]);
 
   return (
     <Container>
-      <Snippet />
-      <input
-        type="text"
-        value={currentInput}
-        onChange={event => setCurrentInput(event.target.value)}
-      />
+      <Scoreboard score={currentScore} />
+      {currentSnippetKey && (
+        <Snippet snippet={{ [currentSnippetKey]: snippets[currentSnippetKey] }} />
+      )}
+      <input ref={inputEl} type="text" />
     </Container>
   );
 };
