0b8fe0fc714a06b2aefd9e38ab8065ddcec2dc03
Haakan <haakandev@gmail.com>
2019-09-28 19:19:47 +1000

    add input and snippet component

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Snippet } from '../components';
 
 const Game = () => {
-  return <div>Game...</div>;
+  const [currentInput, setCurrentInput] = useState('');
+
+  return (
+    <Container>
+      <Snippet />
+      <input
+        type="text"
+        value={currentInput}
+        onChange={event => setCurrentInput(event.target.value)}
+      />
+    </Container>
+  );
 };
 
 export default Game;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Home.module.css';
+import { Container } from '../components';
 import logo from '../assets/logo.svg';
 
 const Home = ({ onStart }) => {
   return (
-    <div className={styles.home}>
+    <Container>
       <img src={logo} className={styles.homeLogo} alt="logo" />
       <button type="button" className={styles.homeButton} onClick={onStart}>
         Start Training
       </button>
-    </div>
+    </Container>
   );
+};
+
+Home.propTypes = {
+  onStart: PropTypes.func.isRequired,
 };
 
 export default Home;
