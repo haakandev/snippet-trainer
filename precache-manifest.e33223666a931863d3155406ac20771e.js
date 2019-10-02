e7a2181bda764808c604a9338391d4571d46cf39
Haakan <haakandev@gmail.com>
2019-10-02 16:44:17 +1000

    Updates

---
+++ precache-manifest.e33223666a931863d3155406ac20771e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f72d42552a33e16c51b1b23bbb484e",
+    "url": "/snippet-trainer/index.html"
+  },
+  {
+    "revision": "2dac706095f5be552bf4",
+    "url": "/snippet-trainer/static/css/main.6fdd91ba.chunk.css"
+  },
+  {
+    "revision": "3566024420c861fec4c6",
+    "url": "/snippet-trainer/static/js/2.7a047dfd.chunk.js"
+  },
+  {
+    "revision": "2dac706095f5be552bf4",
+    "url": "/snippet-trainer/static/js/main.29e0fca1.chunk.js"
+  },
+  {
+    "revision": "bb348a9153f7e6c298a7",
+    "url": "/snippet-trainer/static/js/runtime-main.ea18cbcc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/snippet-trainer/static/media/logo.25bf045c.svg"
+  }
+]);
