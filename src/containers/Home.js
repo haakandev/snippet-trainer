66ece3c98fb1f35242756d7a9b9a0a2b656f5a9e
Haakan <haakandev@gmail.com>
2019-10-04 21:07:49 +1000

    remove react logo

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,12 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Home.module.css';
 import { Container } from '../components';
-import logo from '../assets/logo.svg';
 
 const Home = ({ onStart }) => {
   return (
     <Container center>
-      <img src={logo} className={styles.homeLogo} alt="logo" />
       <button type="button" className={styles.homeButton} onClick={onStart}>
         Start Training
       </button>
